4e8554bfb6dd7cad4a005de0a1dd81b7a1f9b6d3
jesstheleftie <jessetang57@hotmail.com>
2024-10-28 15:38:21 -0700

    finish homework

--- solutions.js
+++ solutions.js
@@ -18,12 +18,83 @@
 
 //Excercise 3
 
-const car = {
-  make: "Audi",
-  model: "q5",
+// const car = {
+//   make: "Audi",
+//   model: "q5",
+// };
+
+// const { make, model } = car;
+
+// console.log(make);
+// console.log(model);
+
+//Excercise 4
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const duplicatePizzaToppings = [...pizzaToppings];
+// console.log(duplicatePizzaToppings)
+// const controversialPizzaToppings = duplicatePizzaToppings;
+
+// console.log(controversialPizzaToppings)
+
+//Excercise 5
+
+// const car = {
+//   make: "Audi",
+//   model: "q5",
+// };
+
+// const clonedCar = { ...car };
+// console.log("Clone:", clonedCar);
+
+// clonedCar.model = "q7";
+// console.log(clonedCar);
+
+//Excercise 6
+
+// const userProfile = {
+//   propertyName: "jesstheleftie",
+// };
+
+// const selectedUser = "propertyName";
+// const selectedUserInput = userProfile[selectedUser];
+// console.log(selectedUserInput);
+
+//Excercise 7
+//import and export files created
+
+//Excercise 8
+// function describePet(noun = "cat", adjective = "white") {
+//   console.log(`The ${noun} is ${adjective}.`);
+// }
+
+// describePet();
+// describePet("dog", "black");
+
+//Excercise 9
+
+// let pizza = 'tasty';
+
+// pizza = 'tasty' ? console.log('yum') : console.log('yuck')
+
+//Excercise 10
+
+// const localLangConfig = null;
+
+// const lang = localLangConfig || "en";
+
+// console.log("Language Setting", lang);
+
+// const userSaveTheme = null;
+
+// const userTheme = userSaveTheme || "light";
+
+// console.log("User theme setting", userTheme);
+
+//Excercise 11
+const adventurer = {
+  name: "Alice",
 };
+let cat = adventurer.cat?.age;
 
-const { make, model } = car;
-
-console.log(make);
-console.log(model);
+console.log(cat);

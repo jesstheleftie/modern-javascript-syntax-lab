51160ca2a48ec0755bb6fdc1b31bdf95d1e4fed1
jesstheleftie <jessetang57@hotmail.com>
2024-10-28 13:33:36 -0700

    complete solutions 2 to 4

--- solutions.js
+++ solutions.js
@@ -1,9 +1,29 @@
 // Excercise 1
 
-const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+// const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
-const newNums = nums.map((num) => {
-  return num * 2;
-});
+// const newNums = nums.map((num) => {
+//   return num * 2;
+// });
 
-console.log(newNums);
+// console.log(newNums);
+
+//Excercise 2
+
+// const pizzaToppings = ["Pineapple", "Olives", "Anchovies"];
+// const [firstTopping, secondTopping] = pizzaToppings;
+
+// console.log(firstTopping);
+// console.log(secondTopping);
+
+//Excercise 3
+
+const car = {
+  make: "Audi",
+  model: "q5",
+};
+
+const { make, model } = car;
+
+console.log(make);
+console.log(model);
